b6413d6dba08ae4cffad47d8d761d916af8c71fb
VEA22 <cj9745@naver.com>
2021-01-10 05:31:49 +0900

    5, modify html -> php, add Google Search

--- index.php
+++ index.php
@@ -32,7 +32,7 @@
         <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
             <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
                 <div class="sidebar-brand-icon rotate-n-15">
                     <i class="fas fa-pencil-alt fa-lg"></i>
                 </div>
@@ -44,14 +44,14 @@
 
             <!-- Nav Item - Introduce -->
             <li class="nav-item active">
-                <a class="nav-link" href="index.html">
+                <a class="nav-link" href="index.php">
                     <i class="fas fa-fw fa-cat"></i>
                     <span>Introduce</span></a>
             </li>
 
             <!-- Nav Item - Tutorial -->
             <li class="nav-item active">
-                <a class="nav-link" href="index.html">
+                <a class="nav-link" href="index.php">
                     <i class="fas fa-fw fa-dog"></i>
                     <span>Tutorial</span></a>
             </li>
@@ -115,6 +115,21 @@
                     <i class="fas fa-fw fa-clock"></i>
                     <span>Everytime</span></a>
             </li>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider d-none d-md-block">
+
+            <!-- Search Google -->
+            <form method=get action="http://www.google.co.kr/search" target="_blank" >
+              <table bgcolor="#224abe">
+                <tr>
+                  <td>
+                    <input type=text name=q size=25 maxlength=255 value="" /> <!-- 구글 검색 입력 창 -->
+                    <input type=submit name=btnG value="Google 검색" /> <!-- 검색 버튼 -->
+                  </td>
+                </tr>
+              </table>
+            </form>
 
             <!-- Divider -->
             <hr class="sidebar-divider d-none d-md-block">
